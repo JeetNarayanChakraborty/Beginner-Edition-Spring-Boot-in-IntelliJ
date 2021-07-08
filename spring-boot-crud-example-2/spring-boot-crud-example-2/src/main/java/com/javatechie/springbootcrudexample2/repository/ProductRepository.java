bf2720e418d3e39f9a40e1f943912ac97d9ebf57
Nikita Rath <54584131+nikita9604@users.noreply.github.com>
2021-07-08 13:54:45 +0530

    Delete spring-boot-crud-example-2/spring-boot-crud-example-2 directory

--- spring-boot-crud-example-2/spring-boot-crud-example-2/src/main/java/com/javatechie/springbootcrudexample2/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javatechie.springbootcrudexample2.repository;
-
-import com.javatechie.springbootcrudexample2.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer>{
-
-    Product findByName(String name);
-}
