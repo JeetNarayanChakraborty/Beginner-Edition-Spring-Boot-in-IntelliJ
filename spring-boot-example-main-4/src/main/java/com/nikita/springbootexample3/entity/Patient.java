d822952d68e0cca0c58341385bb48e0aec69f8bb
Nikita Rath <54584131+nikita9604@users.noreply.github.com>
2021-07-08 14:00:14 +0530

    Add files via upload

---
+++ spring-boot-example-main-4/src/main/java/com/nikita/springbootexample3/entity/Patient.java
@@ -0,0 +1,23 @@
+package com.nikita.springbootexample3.entity;
+
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Patient {
+    @Id
+    private int pid;
+    private String name;
+    private int age;
+}
